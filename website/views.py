484fdbb88f74ae60e811b733cdba64ce87560173
Cocoa Milka <jrgbk4@umsystem.edu>
2023-05-13 06:23:36 -0500

    Added status attribute for commissions

--- website/views.py
+++ website/views.py
@@ -16,13 +16,14 @@ def home():
         title = request.form.get('title')
         price = request.form.get('price')
         description = request.form.get('description')
+        status = request.form.get('status')
 
         if len(title) < 1:
             flash('Title cannot be empty!', category='error')
         elif len(description) < 1:
             flash('Description cannot be empty!', category='error')
         else:
-            new_commission = Commission(title=title, price=price, commissioner=current_user.id, description=description)
+            new_commission = Commission(title=title, price=price, commissioner=current_user.id, description=description, status=status)
             db.session.add(new_commission)
             db.session.commit()
             flash('Commission added!', category='success')
